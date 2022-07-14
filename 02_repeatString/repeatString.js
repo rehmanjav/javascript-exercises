96b19d9c3bd5f07105f39fa28c8d85d5cb90c1cd
Rehman Javaid <rehmanjav@outlook.com>
2022-07-14 17:16:31 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string="", repeat) {
+    if (repeat < 0) {return "ERROR";} 
+    if (string === "") {return "";}
 
+    let output = '';
+    for (let i = 0; i < repeat; i++) {
+        output += string;
+    }
+
+    return output;
 };
 
 // Do not edit below this line
