e4263d786cc7f03f09b2e69a6489c0fd3dccacf3
Rehman Javaid <rehmanjav@outlook.com>
2022-07-15 17:52:33 -0400

    Exercise 4 WIP

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let indexLast = arguments.length -1;
+    let output = array;
 
+    for (let i = 1; i <= indexLast; i++) {
+        while (output.includes(arguments[i])) {
+            output.slice(output.indexOf(arguments[i]), 1);
+        }
+
+
+    }
+
+    return output;
 };
 
 // Do not edit below this line
