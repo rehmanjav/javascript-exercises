4fbb972d3f98d05eae7c4a4503be7f8977a0d564
Rehman Javaid <rehmanjav@outlook.com>
2022-07-15 15:57:57 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let output = "";
+    let len = str.length;
 
+    for (let i = 1; i <= len; i++) {
+        output += str[len - i];
+    }
+
+    return output;
 };
 
 // Do not edit below this line
