513231b5eaa349c013b4945585f846f7d7d47452
Rehman Javaid <rehmanjav@outlook.com>
2022-07-16 15:50:29 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(faranh) {
+  return +((faranh - 32) * (5 / 9)).toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(celc) {
+  return +((celc * (9 / 5)) + 32).toFixed(1);
 };
 
 // Do not edit below this line
