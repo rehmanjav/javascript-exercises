cdb3dc1edae6d70f6a4870b5a53550fa6a714322
Rehman Javaid <rehmanjav@outlook.com>
2022-07-16 15:29:04 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) return "ERROR";
+    if (typeof a != "number" || typeof b != "number") return "ERROR";
 
+    let start = a < b ? a : b;
+    let end = b > a ? b : a;
+
+    let sum = 0;
+
+    for (let i = start; i <= end; i++ ) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
