290291ea691a4d755adc68cca24e05e97be3dcc0
Rehman Javaid <rehmanjav@outlook.com>
2022-08-02 16:20:27 -0400

    09-palindromes WIP

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,10 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    for (let i = 0; i <= Math.floor((word.length / 2)); i++) {
+        if (word[i] != word[word.length - (i + 1)]) {
+            return false;
+        }
+    }
+    return true;
 
 };
 
