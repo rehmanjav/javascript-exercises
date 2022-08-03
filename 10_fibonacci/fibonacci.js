c72fd294b0f0f6c5d54084d139562ea0ee1e26dd
Rehman Javaid <rehmanjav@outlook.com>
2022-08-03 15:51:08 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,9 @@
 const fibonacci = function(n) {
+    n = +n;
+    if (n <= 0) {return "OOPS";};
     let fibSeq = [1, 1,];
 
-    if (fibSeq.length < n) {
+    while (fibSeq.length < n) {
         fibSeq.push(fibSeq[fibSeq.length - 1] + fibSeq[fibSeq.length - 2]);
     }
 
