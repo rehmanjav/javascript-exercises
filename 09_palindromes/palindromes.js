73b95d45cf3b26802d423dc39288acd9ab37f97f
Rehman Javaid <rehmanjav@outlook.com>
2022-08-03 15:45:40 -0400

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
 const palindromes = function (word) {
+    if (word[word.length - 1] == "." ||
+        word[word.length - 1] == "?" ||
+        word[word.length - 1] == "!") {
+            word = word.slice(0, -1);
+    }
+
+    word = word.toLowerCase();
+    word = word.replaceAll(" ", "");
+    word = word.replaceAll(",", "");
+
     for (let i = 0; i <= Math.floor((word.length / 2)); i++) {
         if (word[i] != word[word.length - (i + 1)]) {
             return false;
