9885f87219d0cb1e214f049f3a9b0646549afc7a
Rehman Javaid <rehmanjav@outlook.com>
2022-08-03 17:13:21 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    
+    let ageArray = array.map(person => {
+        let name = person.name;
+        let birth = person.yearOfBirth;
+        let death;
 
+        if (!person.yearOfDeath) {
+            death = new Date().getFullYear();
+        } else {
+            death = person.yearOfDeath;
+        }
+
+        let age = death - birth;
+
+        return {name: name, age: age };
+    });
+
+  
+
+    ageArray.sort((a, b) => {
+        if (a.age > b.age) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    return ageArray[ageArray.length - 1];
 };
 
 // Do not edit below this line
