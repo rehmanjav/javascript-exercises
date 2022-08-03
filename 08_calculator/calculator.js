b7ee6d30ab8114b1d5c5bcb40ce6ff2bcc47186c
Rehman Javaid <rehmanjav@outlook.com>
2022-08-03 15:30:11 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,16 +6,18 @@ const subtract = function(a, b) {
 	return a - b;
 };
 
-const sum = function(...args) {
+const sum = function(nums) {
 	let sum = 0;
-  for (let num in args) {
-    sum += num;
-  }
+  nums.forEach(num => sum += num);
   return sum;
 };
 
-const multiply = function(a, b) {
-  return a * b;
+const multiply = function(nums) {
+  let first = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    first *= nums[i];
+  }
+  return first;
 };
 
 const power = function(a, b) {
@@ -23,6 +25,7 @@ const power = function(a, b) {
 };
 
 const factorial = function(n) {
+  if (n == 0) {return 1;};
   let factorial = n;
 	for (let i = n-1; i > 1; i--) {
     factorial *= i;
